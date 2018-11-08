5b5f0ce41bf3ee778abee3359019b84634fffff8
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-11-08 19:06:44 +0000

    GameForm stateful -> stateless

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,48 +1,37 @@
 import React from 'react';
 import './GameForm.css';
 
-export default class GameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    
-  // Props received:
-  //  minGuess: Number
-  //  maxGuess: Number
-  //  submitAction: function(val)
-    
-  }
+export default function GameForm(props) {
 
-  
-  onSubmit(event) {
+  function onSubmit(event) {
     event.preventDefault();
   
-    const guess = this.guessInput.value;
+
+    const guess = event.target.elements.guess.value;
     
     // Execute callback
-    this.props.submitAction(guess);
+    props.submitAction(guess);
 
     // Reset form
-    this.guessInput.value ='';
+    event.target.elements.guess.value = '';
 
   }
-  
-  render() {
-  
+
     return (
    
-      <form className="game-form" onSubmit={(e) => this.onSubmit(e)}>
-          {this.props.submitVisible && 
+      <form className="game-form" onSubmit={(e) => onSubmit(e)}>
+          {props.submitVisible && 
             <div className="form-elements">
               <input type="number"
-                     min={this.props.minGuess}
-                     max={this.props.maxGuess}
-                     ref={input => this.guessInput = input} required />
+                     min={props.minGuess}
+                     max={props.maxGuess}
+                     name="guess"
+                     required />
               <input type="submit" value="Guess!"/>
             </div>
           }
       </form>
     
     );    
-  }
- 
+
 }
