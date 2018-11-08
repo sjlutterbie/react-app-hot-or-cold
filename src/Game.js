99794133e8b70366e161d16c6a4734c46e53a74f
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-11-08 17:23:46 +0000

    Successfully updating state.guessList

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './Game.css';
 
+import GameForm from './components/GameForm';
+
 class Game extends Component {
   // Game component owns the primary state, which it passses to the
   //  sub-components as props
   
   constructor(props) {
     super(props);
+    
     this.state = {
       // Target number is set when the game is loaded
       secretNumber: Math.floor(Math.random() * 100)+ 1,
@@ -14,11 +17,24 @@ class Game extends Component {
       guessList: [],
       // How close must a guess be to earn "Hot" feedback?
       hotRange: 10,
+      // Set the game range to avoid hard-coding.
+      minGuess: 1,
+      maxGuess: 100,
       // Set the hot/cold feedback to avoid hard-coding.
       hotFeedback: "Hot!",
       coldFeedback: "Cold..."
     };
   }
+  
+  submitGuess(guess) {
+
+    this.setState({
+        guessList: [...this.state.guessList, guess]
+      }
+    );
+    
+  }
+  
 
   // DEV NOTE:
   //  While building the game-form, guess-count is set to render assorted dev
@@ -34,10 +50,8 @@ class Game extends Component {
           <div className="feedback-area">
             <p>[Guess a number! | Hot | Cold ]</p>
           </div>
-          <form className="game-form">
-            <input type="number" min="1" max="100" required/>
-            <input type="submit" value="Guess!"/>
-          </form>
+          <GameForm minGuess={this.state.minGuess} maxGuess={this.state.maxGuess}
+            submitAction={guess => this.submitGuess(guess)}/>
           <div className="guess-count">
             <p>guessList: {this.state.guessList}</p>
           </div>
---
+++ src/components/GameForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './GameForm.css';
+
+export default class GameForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+  // Props received:
+  //  minGuess: Number
+  //  maxGuess: Number
+  //  submitAction: function(val)
+  
+  onSubmit(event) {
+    event.preventDefault();
+  
+    const guess = this.guessInput.value;
+    
+    this.props.submitAction(guess);
+
+  }
+  
+  render() {
+  
+    return (
+    
+      <form className="game-form" onSubmit={(e) => this.onSubmit(e)}>
+        <input type="number" min={this.props.minGuess} max={this.props.maxGuess}
+               ref={input => this.guessInput = input} required />
+        <input type="submit" value="Guess!"/>
+      </form>
+    
+    );    
+  }
+ 
+}
